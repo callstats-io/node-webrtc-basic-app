4c89ab51bc9e457cf50346048dd3149dd0dc1692
marcinag <marcin@callstats.io>
2015-10-05 14:59:06 +0300

    Delete config.js

--- app/config.js
+++
@@ -1,8 +0,0 @@
-AppConfiguration = function() {
-  //var appId = "175240362";
-  //var appSecret = "Gvd126EUWQheaWQX9mwmeWIbzvs=";
-  var appId = "497346896";
-  var appSecret = "usKVYjMm3JoqGhILunSSgfJynYs=";
-
-  return {"appId":appId,"appSecret":appSecret};
-}
