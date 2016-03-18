820edc49e356d8c7a4398009fe44b4363c17a2f1
karthikbr82 <karthik@callstats.io>
2016-03-18 14:02:33 +0200

    getstats and dailystatstest param added

--- app/webrtc_app.js
+++ app/webrtc_app.js
@@ -1,6 +1,8 @@
 
 var localVideo;
+var localScreenShare;
 var localStream;
+var localScreenStream;
 var userPCs = [];
 var isChannelReady = false;
 var calls;
@@ -8,20 +10,28 @@ var calls;
 var isScreenSharingOn = false;
 var isFirefox = false;
 var isChrome = false;
+var statsFromBrowser;
+var inBoundLocalStatsKeys;
+var outBoundLocalStatsKeys;
+var inBoundRemoteStatsKeys;
+var outBoundRemoteStatsKeys;
+var pcKeys;
 
 var room = 'foo';
 console.log("Room is " + room);
 
 var temp = Math.floor(Math.random()*10000);
 var myUserId = temp.toString();
+var browserInfo = detectBrowserInfo();
+console.log("Browser details ",browserInfo);
 
-var constraints = {
-  audio: true,
-  video: true
+var codeBaseType = {
+  chrome: "Chrome",
+  firefox: "Firefox",
+  edge: "Edge"
 };
 
 var socket = io.connect();
-
 
 if(window.navigator.userAgent.match('Chrome')) {
   isChrome = true;
@@ -32,6 +42,32 @@ else if (window.navigator.userAgent.match('Firefox')) {
   isFirefox = true;
 }
 
+if(isChrome) {
+  constraints = {
+    audio: {
+      mandatory: {
+      googEchoCancellation: true, // disabling audio processing
+      googAutoGainControl: true,
+      googNoiseSuppression: true,
+      googHighpassFilter: true,
+      googTypingNoiseDetection: true
+      },
+      optional: [{ echoCancellation: true}] },
+    video: {mandatory: {/*minFrameRate: 20000*/}, optional: []}
+  };
+} else {
+  constraints = {
+    audio: true,
+    video: {width: 4024, height: 4024},
+  };
+}
+
+if(window.location.pathname === "/dailystatstest") {
+  console.log("Daily stats test, enable fake mozila");
+  if(!isChrome) {
+    constraints.fake = true;
+  }
+}
 
 if (room !== ''){
   console.log('participant', room,myUserId);
@@ -81,7 +117,6 @@ function getMinQuality (quality) {
   return retQualityString;
 }
 
-
 function statsCallback (stats){
   console.log("processed stats ",stats);
   var $bitrate = $('#bitrate');
@@ -130,6 +165,331 @@ function csReportErrorCallback (err, msg){
   console.log("CallStats error: err="+err+" msg="+msg);
 }
 
+function parseStats(obj) {
+  var statsString = '';
+  statsString += '{"Timestamp":"';
+  if (obj.timestamp instanceof Date) {
+    statsString += obj.timestamp.getTime().toString();
+  } else {
+    statsString += obj.timestamp;
+  }
+  statsString += '",';
+  if (obj.type) {
+    statsString += ('"type" : "' +  obj.type + '",');
+  }
+
+  var i = 0;
+  if (obj.names) {
+    var names = obj.names();
+    // statsString += 'names=[';
+    for (i = 0; i < names.length; ++i) {
+      statsString += '"';
+      statsString += names[i];
+      statsString += '" : "';
+      statsString += obj.stat(names[i]);
+      statsString += '"';
+      if (i + 1 !== names.length) {
+        statsString += ',';
+      }
+    }
+    // statsString += ']';
+  } else {
+    var length = Object.keys(obj).length;
+    i = 0;
+    var key;
+    for (key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        i++;
+        if (key !== "timestamp") {
+          statsString += '"';
+          statsString += key;
+          statsString += '" : "';
+          statsString += obj[key];
+          statsString += '"';
+          if (i < length) {
+            statsString += ',';
+          }
+        }
+      }
+    }
+  }
+  statsString += '}';
+  return statsString;
+}
+
+ function statsClassifier(obj, codeBase) {
+  var retObj = { };
+  if (codeBase === codeBaseType.firefox) {
+    if (obj.type === "inboundrtp" || obj.type === "outboundrtp") {
+      retObj.ssrc = obj.ssrc;
+      retObj.inbound = (obj.type === "inboundrtp") ? true : false;
+      retObj.data = obj;
+      retObj.mediaType = obj.mediaType;
+      retObj.reportType = (obj.isRemote === "true") ? "remote" : "local";
+    } else if (obj.type === "candidatepair" && obj.selected) {
+      retObj.Transport = obj;
+    } else if (obj.type === "localcandidate") {
+      retObj.localCandidate = obj;
+    } else if (obj.type === "remotecandidate") {
+      retObj.remoteCandidate = obj;
+    }
+  } else if(codeBase === codeBaseType.chrome) {
+    if (obj.type === "ssrc") {
+      retObj.reportType = "local";
+      if (obj.bytesSent) {
+        retObj.inbound = false;
+      } else {
+        //retObj.reportType = "remote";
+        retObj.inbound = true;
+      }
+      retObj.ssrc = obj.ssrc;
+      retObj.data = obj;
+    } else if (obj.type === "googCandidatePair") {
+      retObj.Transport = obj;
+    } else if (obj.type === "VideoBwe") {
+      retObj.bwe = obj;
+    }
+  } else if(codeBase === codeBaseType.edge) {
+    if(obj.type === "inbound-rtp" || obj.type === "outbound-rtp") {
+      retObj.ssrc = obj.ssrc;
+      retObj.inbound = (obj.type === "inbound-rtp") ? true : false;
+      retObj.data = obj;
+      retObj.reportType = "local";
+    } else if(obj.type === "transport") {
+      retObj.Transport = obj;
+    }
+  }
+  return retObj;
+}
+
+  function isEmptyDict(ob) {
+    return $.isEmptyObject(ob);
+  }
+
+/**
+ * @memberOf callstats
+ * creates the handler to handle the fabric stats
+ */
+function createFabricStatsHandler(localID, remoteID, conferenceID, pc) {
+  function onFabricStats(stats) {
+    var results = [];
+    var _statsString;
+    var _statJSON;
+    var codeBase = browserInfo.codebase;
+    var localCandidates = {}, remoteCandidates = {};
+    var _statsTuple = { "streams": { } };
+
+    console.log("In createFabricStatsHandler");
+
+    if (codeBase === codeBaseType.firefox && browserInfo.name !== "Safari") {
+      results = [];
+      stats.forEach(function (item) {
+        results.push(item);
+      });
+    } else if(codeBase === codeBaseType.chrome && browserInfo.name !== "Safari") {
+      results = stats.result();
+    } else {
+      results = [];
+      for(var k in stats) {
+        results.push(stats[k]);
+      }
+    }
+
+    for (var i = 0; i < results.length; ++i) {
+      _statsString = parseStats(results[i]);
+      _statJSON = statsClassifier(JSON.parse(_statsString), codeBase);
+      if (!isEmptyDict(_statJSON)) {
+        if (_statJSON.hasOwnProperty("Transport")) {
+          key = "Transport";
+          if (!(_statsTuple.hasOwnProperty(key))) {
+            _statsTuple[key] = [];
+          }
+          if (codeBase !== codeBaseType.firefox) {
+            if (parseInt(_statJSON[key].bytesReceived, 10)  > 0 || parseInt(_statJSON[key].bytesSent, 10) > 0) {
+              _statsTuple[key].push(_statJSON[key]);
+            }
+          } else {
+            _statsTuple[key].push(_statJSON[key]);
+          }
+        } else if (_statJSON.hasOwnProperty("localCandidate")) {
+          localCandidates[_statJSON.localCandidate.id] = _statJSON.localCandidate;
+        } else if (_statJSON.hasOwnProperty("remoteCandidate")) {
+          remoteCandidates[_statJSON.remoteCandidate.id] = _statJSON.remoteCandidate;
+        } else if (_statJSON.hasOwnProperty("bwe")) {
+          _statsTuple.bwe = _statJSON.bwe;
+        } else {
+          if (_statsTuple.streams[_statJSON.ssrc] === undefined) {
+            _statsTuple.streams[_statJSON.ssrc] = {};
+          }
+          if (_statJSON.inbound) {
+            if (_statsTuple.streams[_statJSON.ssrc].inbound === undefined) {
+              _statsTuple.streams[_statJSON.ssrc].inbound = { };
+            }
+            inboundStats = _statsTuple.streams[_statJSON.ssrc].inbound;
+            inboundStats[_statJSON.reportType] = { "data": _statJSON.data };
+          } else {
+            if (_statsTuple.streams[_statJSON.ssrc].outbound === undefined) {
+              _statsTuple.streams[_statJSON.ssrc].outbound = { };
+            }
+            _statsTuple.streams[_statJSON.ssrc].outbound[_statJSON.reportType] = { "data": _statJSON.data };
+          }
+        }
+      }
+    }
+
+    statsFromBrowser = {};
+    statsFromBrowser.inBoundLocalStatsKeys = getInboundLocalKeys(_statsTuple.streams);
+    statsFromBrowser.outBoundLocalStatsKeys = getOutboundLocalKeys(_statsTuple.streams);
+
+    statsFromBrowser.inBoundRemoteStatsKeys = getInboundRemoteKeys(_statsTuple.streams);
+    statsFromBrowser.outBoundRemoteStatsKeys = getOutboundRemoteKeys(_statsTuple.streams);
+    statsFromBrowser.browserVersion = browserInfo.ver;
+    statsFromBrowser.peerConnectionKeys = pcKeys;
+
+
+    console.log("Stats tupple ",_statsTuple);
+    console.log("Local stats ",inBoundLocalStatsKeys, outBoundLocalStatsKeys);
+    console.log("Remote stats ",inBoundRemoteStatsKeys, outBoundRemoteStatsKeys);
+
+  }
+  return onFabricStats;
+}
+
+function getKeys(obj) {
+  all = {};
+  function get(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0, l= keys.length; i < l; i++) {
+      var key = keys[i];
+      var value = obj[key];
+      if (value instanceof Object) get(value);
+      else all[key] = true;
+    }
+  }
+  get(obj);
+  return Object.keys(all);
+}
+
+function getOutboundLocalKeys(obj) {
+  all = {};
+  function get(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0, l= keys.length; i < l; i++) {
+      var key = keys[i];
+      if(key !== "inbound" && key !== "remote") {
+        var value = obj[key];
+        if (value instanceof Object) get(value);
+        else all[key] = true;
+      }
+    }
+  }
+  get(obj);
+  return Object.keys(all);
+}
+
+function getInboundLocalKeys(obj) {
+  all = {};
+  function get(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0, l= keys.length; i < l; i++) {
+      var key = keys[i];
+      if(key !== "outbound" && key !== "remote") {
+        var value = obj[key];
+        if (value instanceof Object) get(value);
+        else all[key] = true;
+      }
+    }
+  }
+  get(obj);
+  return Object.keys(all);
+}
+
+function getOutboundRemoteKeys(obj) {
+  all = {};
+  function get(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0, l= keys.length; i < l; i++) {
+      var key = keys[i];
+      if(key !== "inbound" && key !== "local") {
+        var value = obj[key];
+        if (value instanceof Object) get(value);
+        else all[key] = true;
+      }
+    }
+  }
+  get(obj);
+  return Object.keys(all);
+}
+
+function getInboundRemoteKeys(obj) {
+  all = {};
+  function get(obj) {
+    var keys = Object.keys(obj);
+    for (var i = 0, l= keys.length; i < l; i++) {
+      var key = keys[i];
+      if(key !== "outbound" && key !== "local") {
+        var value = obj[key];
+        if (value instanceof Object) get(value);
+        else all[key] = true;
+      }
+    }
+  }
+  get(obj);
+  return Object.keys(all);
+}
+
+function getPeerConnectionKeys(pc) {
+  all = {};
+  for (var prop in pc) {
+    all[prop] = true;
+  }
+  return Object.keys(all);
+}
+
+setInterval(getStats,10000);
+
+function getStats() {
+  var codeBase = browserInfo.codebase;
+  for(var userId in userPCs)
+  {
+    var pc = userPCs[userId].getPeerConnection();
+
+    if (pc) {
+      pcKeys = getPeerConnectionKeys(pc);
+      if (codeBase === codeBaseType.firefox || browserInfo.name === "Safari") {
+        pc.getStats(null, createFabricStatsHandler(myUserId, userId, "foo", pc),
+          function logError(err) {
+            console.log("Error");
+        });
+      } else if(codeBase === codeBaseType.chrome) {
+        pc.getStats(createFabricStatsHandler(myUserId, userId, "foo", pc));
+      } else if(codeBase === codeBaseType.edge) {
+        pc.getStats()
+        .then(createFabricStatsHandler(myUserId, userId, "foo", pc))
+        .catch(function(err) {
+          console.log("Error");
+        });
+      }
+    }
+    userPCs[userId].sendMessage();
+  }
+
+  return ["success","faile"];
+}
+
+function getstats() {
+  console.log("Returning stats ",statsFromBrowser);
+  return statsFromBrowser;
+}
+
+function csReportErrorCallback (err, msg){
+  console.log("CallStats error: err="+err+" msg="+msg);
+}
+
+var params = {
+  //disableBeforeUnloadHandler: false
+};
+
 callStats.initialize(appId, appSecret, myUserId, csInitCallback,statsCallback);
 
 document.getElementById("switchBtn").onclick = switchScreen;
@@ -163,8 +523,11 @@ function switchScreen(){
     }
     if(isScreenSharingOn)
     {
+      document.getElementById("switchBtn").value = 'Start Screen sharing';
+      localScreenStream = null;
+      attachMediaStream(localScreenShare,localScreenStream);
       if (pc) {
-        callStats.sendFabricEvent(pc, callStats.fabricEvent.videoResume, room);
+        callStats.sendFabricEvent(pc, callStats.fabricEvent.screenShareStop, room);
       }
 
       if(isChrome) {
@@ -182,7 +545,7 @@ function switchScreen(){
     else
     {
       if (pc) {
-        callStats.sendFabricEvent(pc, callStats.fabricEvent.videoPause, room);
+        callStats.sendFabricEvent(pc, callStats.fabricEvent.screenShareStart, room);
       }
       if(isChrome){
         removeLocalStream();
@@ -193,6 +556,7 @@ function switchScreen(){
           //socket.emit('participant', room,myUserId);
           isScreenSharingOn = true;
           addLocalStream();
+          document.getElementById("switchBtn").value = 'Stop Screen sharing';
         }
 
       });
@@ -328,26 +692,6 @@ function errorCallback(error){
 }
 
 function doGetUserMedia(callback){
-  if(isChrome) {
-    constraints = {
-      audio: {
-        mandatory: {
-        googEchoCancellation: false, // disabling audio processing
-        googAutoGainControl: true,
-        googNoiseSuppression: true,
-        googHighpassFilter: true,
-        googTypingNoiseDetection: true
-        },
-        optional: [{ echoCancellation: false}] },
-      video: { mandatory: {/*minFrameRate: 20000*/}, optional: [] }
-    };
-  } else {
-    constraints = {
-      audio: true,
-      video: {width: 4024, height: 4024}
-    };
-  }
-
   localVideo = document.querySelector('#localVideo');
   console.log("Do get User Media ",constraints);
   getUserMedia(constraints, function(stream) {
