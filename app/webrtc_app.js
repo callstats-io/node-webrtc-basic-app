86f90497cd750a31f84fd9adb656fe623bf964a8
karthikbr82 <karthik@callstats.io>
2016-11-09 14:45:13 +0200

    detect browser info added

--- app/webrtc_app.js
+++ app/webrtc_app.js
@@ -22,6 +22,142 @@ console.log("Room is " + room);
 
 var temp = Math.floor(Math.random()*10000);
 var myUserId = temp.toString();
+
+/**
+* Detect broswer os, name, version info
+* @memberOf callstats
+* @private
+*/
+function detectBrowserInfo() {
+  var nAgt = navigator.userAgent;
+  var browserName  = navigator.appName;
+  var nVer = navigator.appVersion;
+  var fullVersion  = '' + parseFloat(navigator.appVersion);
+  var verOffset;
+  var codeBase = {
+    goog: "Chrome",
+    moz: "Firefox",
+    plugin: "Plugin",
+    edge: "Edge"
+  };
+
+  var codeBaseType;
+
+  // In Opera, the true version is after "Opera" or after "Version"
+  if ((verOffset = nAgt.indexOf("Opera")) !== -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 6);
+    if ((verOffset = nAgt.indexOf("Version")) !== -1) {
+      fullVersion = nAgt.substring(verOffset + 8);
+    }
+    codeBaseType = codeBase.goog;
+  } else if ((verOffset = nAgt.indexOf("OPR")) !== -1) {
+    browserName = "Opera";
+    fullVersion = nAgt.substring(verOffset + 4);
+    if ((verOffset = nAgt.indexOf("Version")) !== -1) {
+      fullVersion = nAgt.substring(verOffset + 8);
+    }
+    codeBaseType = codeBase.goog;
+  } else if ((verOffset = nAgt.indexOf("MSIE")) !== -1) { // In MSIE, the true version is after "MSIE" in userAgent
+    browserName = "Microsoft Internet Explorer";
+    fullVersion = nAgt.substring(verOffset + 5);
+    codeBaseType = codeBase.goog;
+  } else if((verOffset = nAgt.indexOf("Edge")) !== -1) {
+    browserName = codeBase.edge;
+    fullVersion = nAgt.substring(verOffset + 5);
+    codeBaseType = codeBase.edge;
+  } else if ((verOffset = nAgt.indexOf("Chrome")) !== -1) { // In Chrome, the true version is after "Chrome"
+    browserName = codeBase.goog;
+    fullVersion = nAgt.substring(verOffset + 7);
+    codeBaseType = codeBase.goog;
+  } else if ((verOffset = nAgt.indexOf("Safari")) !== -1) { // In Safari, the true version is after "Safari" or after "Version"
+    browserName = "Safari";
+    fullVersion = nAgt.substring(verOffset + 7);
+    if ((verOffset = nAgt.indexOf("Version")) !== -1) {
+      fullVersion = nAgt.substring(verOffset + 8);
+    }
+    codeBaseType = codeBase.goog;
+  } else if ((verOffset = nAgt.indexOf("Firefox")) !== -1) { // In Firefox, the true version is after "Firefox"
+    browserName = "Firefox";
+    fullVersion = nAgt.substring(verOffset + 8);
+    codeBaseType = codeBase.moz;
+  } else if ((verOffset = nAgt.indexOf("Trident")) !== -1) { // IE 11 has no MSIE
+    browserName = "Microsoft Internet Explorer";
+    verOffset = nAgt.indexOf("rv"); // In IE11, the true version is after "rv"
+    fullVersion = nAgt.substring(verOffset + 3, verOffset + 7);
+    codeBaseType = codeBase.goog;
+  }
+
+  // system
+  // source: http://jsfiddle.net/ChristianL/AVyND/
+  var osName = null;
+  var clientStrings = [
+    {s: 'Windows 3.11', r: /Win16/},
+    {s: 'Windows 95', r: /(Windows 95|Win95|Windows_95)/},
+    {s: 'Windows ME', r: /(Win 9x 4.90|Windows ME)/},
+    {s: 'Windows 98', r: /(Windows 98|Win98)/},
+    {s: 'Windows CE', r: /Windows CE/},
+    {s: 'Windows 2000', r: /(Windows NT 5.0|Windows 2000)/},
+    {s: 'Windows XP', r: /(Windows NT 5.1|Windows XP)/},
+    {s: 'Windows Server 2003', r: /Windows NT 5.2/},
+    {s: 'Windows Vista', r: /Windows NT 6.0/},
+    {s: 'Windows 7', r: /(Windows 7|Windows NT 6.1)/},
+    {s: 'Windows 8.1', r: /(Windows 8.1|Windows NT 6.3)/},
+    {s: 'Windows 8', r: /(Windows 8|Windows NT 6.2)/},
+    {s: 'Windows 10', r: /(Windows 10|Windows NT 10.0)/},
+    {s: 'Windows NT 4.0', r: /(Windows NT 4.0|WinNT4.0|WinNT|Windows NT)/},
+    {s: 'Windows ME', r: /Windows ME/},
+    {s: 'Android', r: /Android/},
+    {s: 'Open BSD', r: /OpenBSD/},
+    {s: 'Sun OS', r: /SunOS/},
+    {s: 'Linux', r: /(Linux|X11)/},
+    {s: 'iOS', r: /(iPhone|iPad|iPod)/},
+    {s: 'Mac OS X', r: /Mac OS X/},
+    {s: 'Mac OS', r: /(MacPPC|MacIntel|Mac_PowerPC|Macintosh)/},
+    {s: 'QNX', r: /QNX/},
+    {s: 'UNIX', r: /UNIX/},
+    {s: 'BeOS', r: /BeOS/},
+    {s: 'OS/2', r: /OS\/2/},
+    {s: 'Search Bot', r: /(nuhk|Googlebot|Yammybot|Openbot|Slurp|MSNBot|Ask Jeeves\/Teoma|ia_archiver)/}
+  ];
+
+  var id, cs;
+  for (id in clientStrings) {
+    cs = clientStrings[id];
+    if (cs.r.test(nAgt)) {
+      osName = cs.s;
+      break;
+    }
+  }
+
+  var osVersion = null;
+  if (/Windows/.test(osName)) {
+    osVersion = /Windows (.*)/.exec(osName)[1];
+    osName = 'Windows';
+  }
+
+  switch (osName) {
+  case 'Mac OS X':
+    osVersion = /Mac OS X (10[\.\_\d]+)/.exec(nAgt)[1];
+    break;
+  case 'Android':
+    osVersion = /Android ([\.\_\d]+)/.exec(nAgt)[1];
+    break;
+  case 'iOS':
+    osVersion = /OS (\d+)_(\d+)_?(\d+)?/.exec(nVer);
+    osVersion = osVersion[1] + '.' + osVersion[2] + '.' + (osVersion[3] | 0);
+    break;
+  }
+
+  return {name: browserName,
+          ver: fullVersion.toString(),
+          os: osName,
+          osVersion: osVersion,
+          codebase: codeBaseType,
+          userAgent: nAgt,
+    };
+}
+
 var browserInfo = detectBrowserInfo();
 console.log("Browser details ",browserInfo);
 
